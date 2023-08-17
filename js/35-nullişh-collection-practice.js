e03ec5e2c828f313c3ebed1e8fa263d60ac574ab
fsoylu <fsmorex1@gmail.com>
2023-08-17 23:31:25 +0300

    11.08.2023

---
+++ js/35-nullişh-collection-practice.js
@@ -0,0 +1,14 @@
+const resultEl = documant.querySelection("#result");
+tax{
+    total:1000,
+}
+
+function alanHesapla( ) {
+    const yuksekklik = undefined;
+    const genişlik = 1000 ;
+     
+    const result = (yukseklik ?? 100) * (genişlik **100);
+
+
+}
+
