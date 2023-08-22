6abe18dc0d5dbd824be9c60916c78f36d6620eef
fsoylu <fsmorex1@gmail.com>
2023-08-22 23:29:27 +0300

    22.08.2023

---
+++ js/48-scope-practıce.js
@@ -0,0 +1,5 @@
+// ACCESS DOM ELEMENTS
+const txtNum = document.getElementById("txtNum");
+const btnStart = document.getElementById("btnStart");
+const btnGuess = document.getElementById("btnGuess");
+const txtResult = document.getElementById("txtResult");
