9382497f4e7b960d1b5506e0e54894dcd7dabf95
fsoylu <fsmorex1@gmail.com>
2023-08-29 23:22:38 +0300

    29.08.2023

---
+++ js/84-timeout.js
@@ -0,0 +1,22 @@
+const sayHello5 = () => {
+    console.log("Hello 5 saniye sonra calisacak fonksiyon");
+};
+
+// geriye timeout Id'si dondurur, dondurulen bu id ile timeout durdurulabilir
+const timeoutId = setTimeout(sayHello5, 5000);
+console.log("Once ben calisirim");
+
+setTimeout(() => {
+    document.querySelector("h1").innerText = "3 saniye sonra calisti"
+}, 3000);
+
+// sure girilmeden de yazilabili
+setTimeout(() => {
+    console.log("Burasi butun JavaScript kodlari calistiktan sonra calisir. Cunku timeout yapisi asenkron calisir.")
+});
+
+console.log("Ikinci ben calisirim");
+
+console.log(timeoutId);
+
+clearTimeout(timeoutId);
